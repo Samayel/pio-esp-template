05ed5a7415894688885d6ceb33ffbc1fa01af076
Markus John <johm@quyo.de>
2020-12-06 15:09:13 +0100

    switch to (almost) empty loop()

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 // Uncomment this line if you want to use the code with freertos only on the ESP32; has to be done before including "OTA.h"
 #define ESP32_RTOS
-                   
+
 #include <Arduino.h>
 #include <Credentials.h>
 #include <OTA.h>
@@ -21,12 +21,14 @@ void loop()
 {
   handleOTA();
 
-  getNTPtime();
-  showTime(timeinfo);
-  delay(1000);
+  // getNTPtime();
+  // showTime(timeinfo);
+  // delay(1000);
 
   // digitalWrite(LED_BUILTIN, HIGH);
   // delay(1000);
   // digitalWrite(LED_BUILTIN, LOW);
   // delay(1000);
+
+  delay(1000);
 }
